7d480d30152d934fc453680b6342e8989c5bd48e
AngrySoilder <chaudhari041@outlook.com>
2020-02-06 18:18:52 +0530

    added write package call

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
-import { readFileSync, existsSync, readdirSync } from 'fs';
+import { readFileSync, existsSync, readdirSync, writeFileSync } from 'fs';
 import * as readline from 'readline-sync';
+import { execSync } from 'child_process';
 import { Terminal } from 'tscolor';
 
 const terminal = new Terminal();
@@ -21,7 +22,7 @@ export function conformInput(prompt: string, defaults?: any): any {
     let negated = false;
     let once = false;
     prompt = askString(prompt, defaults);
-    prompt = prompt + ' :';
+    prompt = prompt + ' : ';
 
     while (!answer.length) {
         if (!!negated && !once) {
@@ -63,4 +64,10 @@ export function askString(question: string, defualt?: string): string {
 export function dirname() {
     const dirs = process.cwd().split('/');
     return dirs[dirs.length - 1];
+}
+
+export function writepackage(pack: any) {
+    // TODO: Forgot to open file ha ha
+    writeFileSync('package.json', JSON.stringify(pack), 'utf-8');
+    execSync('npm install  --package-lock');
 }
