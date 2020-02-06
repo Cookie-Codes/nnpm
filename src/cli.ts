c47893e62f825751551df43fab06733fff389f6e
AngrySoilder <chaudhari041@outlook.com>
2020-02-06 13:29:25 +0530

    Removed Commander

--- src/cli.ts
+++ src/cli.ts
@@ -3,10 +3,6 @@
  */
 
 import * as readline from 'readline';
-import { packageExist, listFiles } from './util';
-import * as program from 'commander';
-
-program.version('1.0.0');
 
 const rl = readline.createInterface({
     input: process.stdin,
