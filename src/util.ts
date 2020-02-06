1b20bdbb2f7fde8d8b19bebe5688a735138a7ab1
AngrySoilder <chaudhari041@outlook.com>
2020-02-06 14:59:26 +0530

    Added Terminal & Prompt

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,13 @@
 import { readFileSync } from 'fs';
 import * as readline from 'readline';
+import { Terminal } from 'tscolor';
+
+const terminal = new Terminal();
+const warning = terminal
+    .tag('repeat')
+    .bold()
+    .withhex('#ff0000');
+
 export function packageExist(): boolean {
     // TODO: Check if Package Exists
 }
@@ -8,7 +16,21 @@ export function listFiles() {
     // TODO: List Files Here & Return
 }
 
-export function conformInput(handler: readline.Interface, prompt: string): any {}
+export function conformInput(handler: readline.Interface, prompt: string): any {
+    let answer: string | Array<string> = '';
+    const negated = false;
+    let once = false;
+
+    while (!!answer.length) {
+        if (negated && !once) {
+            prompt = warning.toString() + prompt;
+            once = true;
+        }
+        handler.question(prompt, ans => {
+            answer = ans;
+        });
+    }
+}
 
 export function getDefaults(): any {
     let values: any = {};
