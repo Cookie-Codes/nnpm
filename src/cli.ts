aab41ff876ba54cc6ad6861e1f075d05e1352a9e
AngrySoilder <chaudhari041@outlook.com>
2020-02-06 13:41:50 +0530

    typo ::

--- src/cli.ts
+++ src/cli.ts
@@ -18,67 +18,54 @@ rl.question(`name [${defualts.name}]`, answer => {
     storage.name = answer;
 });
 
-rl.question('version', answer=>{
-  storage.version = answer;
+rl.question('version', answer => {
+    storage.version = answer;
 });
 
-rl.question('description', answer=>{
-  storage.version = answer;
+rl.question('description', answer => {
+    storage.version = answer;
 });
 
-rl.question('keywords []', answer=>{
-  storage.version = answer;
+rl.question('keywords []', answer => {
+    storage.version = answer;
 });
 
-rl.question('homepage', answer=>{
-  storage.version = answer;
+rl.question('homepage', answer => {
+    storage.version = answer;
 });
 
-rl.question('bugs', answer=>{
-  storage.version = answer;
+rl.question('bugs', answer => {
+    storage.version = answer;
 });
 
-rl.question('license', answer=>{
-  storage.version = answer;
+rl.question('license', answer => {
+    storage.version = answer;
 });
 
-rl.question('author', answer=>{
-  storage.version = answer;
+rl.question('author', answer => {
+    storage.version = answer;
 });
 
-
-rl.question('files: []', answer=>{
-  storage.version = answer;
+rl.question('files: []', answer => {
+    storage.version = answer;
 });
 
-rl.question('version', answer=>{
-  storage.version = answer;
+rl.question('version', answer => {
+    storage.version = answer;
 });
 
-
-rl.question('main: | browser', answer=>{
-  storage.version = answer;
+rl.question('main: | browser', answer => {
+    storage.version = answer;
 });
 
-rl.question('bin', answer=>{
-  storage.version = answer;
+rl.question('bin', answer => {
+    storage.version = answer;
 });
 
-rl.question('repository', answer=>{
-  storage.version = answer;
+rl.question('repository', answer => {
+    storage.version = answer;
 });
 
-rl.question('config', answer=>{
-  storage.version = answer;
+rl.question('config', answer => {
+    storage.version = answer;
 });
-:
-:
-:
-:
-:
-:
-
-
-:
-:
-:
